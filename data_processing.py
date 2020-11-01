d9c88c389405a37bcff50c578ac36eb1b0a90c5f
chijames <tachungc@andrew.cmu.edu>
2020-11-01 14:05:58 -0500

    add code to extract mfcc vectors and dump to data/ directory as pickle format files

--- data_processing.py
+++ data_processing.py
@@ -1,1 +1,22 @@
-"TO DO Data Processing"
+from python_speech_features import mfcc
+import scipy.io.wavfile as wav
+import soundfile as sf
+import os
+import numpy as np
+import pickle
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--data_path", required=True, type=str, help='dir path to ASVSpoof data dir. For example, LA/ASVspoof2019_LA_train/flac/')
+parser.add_argument("--output_path", required=True, type=str, help='file path to output data dir. For example, ./data/train.pkl')
+args = parser.parse_args()
+
+mfcc_feats = []
+for filename in os.listdir(args.data_path):
+    sig, rate = sf.read(os.path.join(args.data_path, filename))
+    mfcc_feat = mfcc(sig,rate)
+    mfcc_feats.append(mfcc_feat)
+
+with open(args.output_path, 'wb') as outfile:
+    pickle.dump(mfcc_feats, outfile)
+
