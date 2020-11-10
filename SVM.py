4e2701392277b166b0e3b47fd2b9323cfa581aef
chijames <tachungc@andrew.cmu.edu>
2020-11-09 22:56:51 -0500

    use first k timesteps for svm

--- SVM.py
+++ SVM.py
@@ -3,6 +3,7 @@ import numpy as np
 import pickle
 import argparse
 from sklearn.svm import SVC
+import matplotlib.pyplot as plt
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--data_path", required=True, type=str, help='path to pickled file. For example, data/train.pkl')
@@ -11,19 +12,26 @@ args = parser.parse_args()
 X = []
 y = []
 
+max_len = 50
+lens = []
 with open(args.data_path, 'rb') as infile:
     data = pickle.load(infile)
     for mfccs, label in data:
-        mfcc = mfccs.mean(0) # sum over all timesteps for now    # timesteps X 13
-        X.append(mfcc)
+        #mfcc = mfccs.mean(0) # sum over all timesteps for now    # timesteps X 13
+        #lens.append(mfccs.shape[0])
+        if len(mfccs) > max_len:
+            mfccs = mfccs[:max_len]
+        elif len(mfccs) < max_len:
+            mfccs = np.concatenate((mfccs, np.array([[0.]*13]*(max_len-len(mfccs)))), axis=0)
+        X.append(mfccs.reshape(-1))
         y.append(label)
-
-clf = SVC(class_weight='balanced')
-#clf = SVC()
-clf.fit(X[:4000], y[:4000])
+X = np.array(X)
+#clf = SVC(class_weight='balanced')
+clf = SVC()
+clf.fit(X, y)
 
 # train acc
 print ('train accuracy:', clf.score(X, y))
 
-with open('svm.pkl', 'wb') as outfile:
+with open('svm_{}.pkl'.format(max_len), 'wb') as outfile:
     pickle.dump(clf, outfile)
--- SVM_test.py
+++ SVM_test.py
@@ -5,19 +5,26 @@ import argparse
 from sklearn.svm import SVC
 
 parser = argparse.ArgumentParser()
-parser.add_argument("--data_path", required=True, type=str, help='path to pickled file. For example, data/train.pkl')
+parser.add_argument("--data_path", required=True, type=str, help='path to pickled file. For example, data/dev.pkl')
 args = parser.parse_args()
 
 X = []
 y = []
+max_len = 50
+lens = []
 with open(args.data_path, 'rb') as infile:
     data = pickle.load(infile)
     for mfccs, label in data:
-        mfcc = mfccs.mean(0)
-        X.append(mfcc)
+        #mfcc = mfccs.mean(0) # sum over all timesteps for now    # timesteps X 13
+        #lens.append(mfccs.shape[0])
+        if len(mfccs) > max_len:
+            mfccs = mfccs[:max_len]
+        elif len(mfccs) < max_len:
+            mfccs = np.concatenate((mfccs, np.array([[0.]*13]*(max_len-len(mfccs)))), axis=0)
+        X.append(mfccs.reshape(-1))
         y.append(label)
 
-with open('svm.pkl', 'rb') as infile:
+with open('svm_{}.pkl'.format(max_len), 'rb') as infile:
     clf = pickle.load(infile)
 
 # predict acc
