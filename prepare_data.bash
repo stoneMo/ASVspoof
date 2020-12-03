f43e6e15faa62529072457133940444d96f7b039
ShentongMo <shentonm@andrew.cmu.edu>
2020-12-04 01:32:27 +0800

    fix typo

--- prepare_data.bash
+++ prepare_data.bash
@@ -1,17 +1,14 @@
 echo "preparing training data"
 python3 data_processing.py --data_path ./LA/ASVspoof2019_LA_train/flac \
-                        --output_path ./data/train_cqcc.pkl \
-                        --label_path ./LA/ASVspoof2019_LA_cm_protocols/ASVspoof2019.LA.cm.train.trn.txt \
-                        --feature_type cqcc
+                        --output_path ./data/train.pkl \
+                        --label_path ./LA/ASVspoof2019_LA_cm_protocols/ASVspoof2019.LA.cm.train.trn.txt
 
 echo "preparing dev data"
 python3 data_processing.py --data_path ./LA/ASVspoof2019_LA_dev/flac \
-                        --output_path ./data/dev_cqcc.pkl \
-                        --label_path ./LA/ASVspoof2019_LA_cm_protocols/ASVspoof2019.LA.cm.dev.trl.txt \
-                        --feature_type cqcc
+                        --output_path ./data/dev.pkl \
+                        --label_path ./LA/ASVspoof2019_LA_cm_protocols/ASVspoof2019.LA.cm.dev.trl.txt 
 
-echo "preparing test data"
-python3 data_processing.py --data_path ./LA/ASVspoof2019_LA_eval/flac \
-                        --output_path ./data/eval_cqcc.pkl \
-                        --label_path ./LA/ASVspoof2019_LA_cm_protocols/ASVspoof2019.LA.cm.eval.trl.txt \
-                        --feature_type cqcc
+# echo "preparing test data"
+# python3 data_processing.py --data_path ./LA/ASVspoof2019_LA_eval/flac \
+#                         --output_path ./data/eval.pkl \
+#                         --label_path ./LA/ASVspoof2019_LA_cm_protocols/ASVspoof2019.LA.cm.eval.trl.txt 
